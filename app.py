4ee2f8495a2069d2b3e2619eff51faad4b13ce4f
adivshahar <adivshahar@gmail.com>
2025-07-23 14:58:08 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    conn = sqlite3.connect("jobs.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT company, position, date_posted FROM jobs ORDER BY date_posted DESC")
+    jobs = [{"company": row[0], "position": row[1], "date_posted": row[2]} for row in cursor.fetchall()]
+    conn.close()
+    return render_template("index.html", jobs=jobs)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+    
